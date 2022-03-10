6d6a18bfc74d9b05bfd348efcd1f218c3d507af8
achille2k <nt.tung@outlook.com>
2022-03-10 13:45:50 +0700

    Update ffmpeg-qsv.sh

--- ffmpeg-qsv.sh
+++ ffmpeg-qsv.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 # Taken from: https://red-full-moon.com/make-hevc-qsv-env-first-half/
 
-# 環境の最新化
+# dotnet-runtime-5.0
+wget https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+sudo dpkg -i packages-microsoft-prod.deb
 sudo apt update
 sudo apt dist-upgrade
-# 必要パッケージのインストール
+sudo apt install apt-transport-https 
+sudo apt update
+sudo apt install dotnet-runtime-5.0
+
+# lib and app
 sudo apt install cmake make autoconf automake libtool g++ bison libpcre3-dev pkg-config libtool libdrm-dev xorg xorg-dev openbox \
 libx11-dev libgl1-mesa-glx libgl1-mesa-dev libpciaccess-dev libfdk-aac-dev libvorbis-dev libvpx-dev libx264-dev libx265-dev \
 ocl-icd-opencl-dev pkg-config yasm libx11-xcb-dev libxcb-dri3-dev libxcb-present-dev libva-dev libmfx-dev intel-media-va-driver-non-free opencl-clhpp-headers git libasound2-dev \
-libmp3lame-dev libpulse-dev
+libmp3lame-dev libpulse-dev libvlc-dev pavucontrol vim
 
-# libvaのインストール
+# libva
 mkdir ~/git && cd ~/git
 git clone https://github.com/intel/libva
 cd libva
@@ -18,7 +24,7 @@ cd libva
 make
 sudo make install
 
-# libva-utilsのインストール
+# libva-utils
 cd ~/git
 git clone https://github.com/intel/libva-utils
 cd libva-utils
@@ -26,7 +32,7 @@ cd libva-utils
 make
 sudo make install
 
-# gmmlibのインストール
+# gmmlib
 cd ~/git
 git clone https://github.com/intel/gmmlib
 cd gmmlib
@@ -35,7 +41,7 @@ cmake -DCMAKE_BUILD_TYPE= Release -DARCH=64 ../
 make
 sudo make install
 
-# Intel-Media-Driverのインストール
+# Intel-Media-Driver
 cd ~/git
 git clone https://github.com/intel/media-driver
 mkdir build_media && cd build_media
@@ -43,13 +49,11 @@ cmake ../media-driver
 make -j"$(nproc)"
 sudo make install
 
-# Intel-Media-Driverで生成されたライブラリをffmpegで使用するために移動
+# Intel-Media-Driver
 sudo mkdir -p /usr/local/lib/dri 
 sudo cp ~/git/build_media/media_driver/iHD_drv_video.so /usr/local/lib/dri/
 
-
-
-# Intel-Media-SDKのインストール
+# Intel-Media-SDK
 cd ~/git
 git clone https://github.com/Intel-Media-SDK/MediaSDK msdk
 cd msdk
@@ -64,7 +68,7 @@ sudo rm -f -- /etc/ld.so.conf.d/imsdk.conf
 echo '/opt/intel/mediasdk/lib' | sudo tee -a /etc/ld.so.conf.d/imsdk.conf
 sudo ldconfig
 
-# 最新版ffmpegの構築
+# ffmpeg
 cd ~/git
 git clone https://github.com/FFmpeg/FFmpeg
 cd FFmpeg
@@ -75,8 +79,30 @@ PKG_CONFIG_PATH=/opt/intel/mediasdk/lib/pkgconfig
 make
 sudo make install
 
-# vaapiが導入されていることを確認 
+# vaapi
 /usr/local/ffmpeg/bin/ffmpeg -hwaccels 2>/dev/null | grep vaapi 
 
-# 利用できるようになったコーデックの確認 
+# vaapi
 /usr/local/ffmpeg/bin/ffmpeg -encoders 2>/dev/null | grep vaapi
+
+sudo apt remove ffmpeg
+
+# set environment
+sudo rm -f -- /etc/environment
+echo 'PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/usr/local/ffmpeg/bin"' | sudo tee -a /etc/environment
+
+
+# custom config /etc/pulse/default.pa
+#load-module module-null-sink sink_name=inputs
+#load-module module-loopback sink=inputs
+#load-module module-loopback sink=inputs
+
+
+# create folder for application
+sudo mkdir -p /opt/gagh
+sudo chown soho.soho /opt/gagh
+sudo mkdir -p /opt/records
+sudo chown soho.soho /opt/records
+
+
+
